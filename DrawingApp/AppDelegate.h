4ba54ab9f15d0f00809c5d16c8476c1b898ec54c
Huda Elhady <huda.elhady@linkdev.com>
2018-08-02 06:35:33 +0200

    Initial Commit

---
+++ DrawingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingApp
+//
+//  Created by MIS on 8/2/18.
+//  Copyright © 2018 Huda Elhady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingApp
+//
+//  Created by MIS on 8/2/18.
+//  Copyright © 2018 Huda Elhady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
